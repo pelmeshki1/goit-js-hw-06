2cff69b1636fca6e84e9814363f8919cf7335ef1
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-16 15:05:37 +0300

    update 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,24 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const ulElement = document.createElement("ul");
-for (const ingredient of ingredients) {
-  const liElment = document.createElement("li");
-  liElment.textContent = ingredient;
-  ulElement.appendChild(liElment);
-}
-document.body.appendChild(ulElement);
+// const ulElement = document.querySelector("#ingredients");
+// for (const ingredient of ingredients) {
+//   const liElment = document.createElement("li");
+//   liElment.textContent = ingredient;
+//   ulElement.appendChild(liElment);
+// }
+// document.body.appendChild(ulElement);
+
+const ulElement = document.querySelector("#ingredients");
+
+console.log(ulElement);
+console.log(ingredients);
+
+const listRefs = ingredients.map((ingredient) => {
+  const listRef = document.createElement("li");
+  listRef.textContent = ingredient;
+  listRef.classList.add("item");
+  return listRef;
+});
+
+ulElement.append(...listRefs);
