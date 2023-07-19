f12ea64616f23aec5c060d31bc4bd237b14fa638
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-19 22:59:31 +0300

    last update fixed

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,15 @@
+// const input = document.querySelector("#font-size-control");
+// const text = document.querySelector("#text");
+// input.value = 16;
+// input.addEventListener("input", fn);
+// function fn(e) {
+//   text.style.fontSize = `${e.target.value}px`;
+// }
+
 const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-input.value = 16;
 input.addEventListener("input", fn);
-function fn(e) {
-  text.style.fontSize = `${e.target.value}px`;
+function fn() {
+  text.style.fontSize = `${input.value}px`;
 }
+fn();
